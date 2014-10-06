15cdbfd33c1efb55edc5c794619f5eb44f5d0f02
Rick Buczynski <rick.buczynski@gmail.com>
2014-10-06 17:26:23 -0400

    prototype display with an imageview that displays an array of pixels

--- RMBBoy/ViewController.m
+++ RMBBoy/ViewController.m
@@ -10,7 +10,7 @@
 #import "Z80.h"
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UIImageView *gameView;
 @end
 
 @implementation ViewController
@@ -18,12 +18,43 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+
+    self.gameView.contentMode = UIViewContentModeScaleAspectFit;
+    UIImage *gameImage = [self imageWidth:15 height:17];
+    self.gameView.image = gameImage;
+    
+    
+    
     Z80_doStuff();
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(UIImage *)imageWidth:(NSUInteger)width height:(NSUInteger)height {
+    /*
+     Assuming pixel color values are 8 bit unsigned
+     
+     You need to create an array that is in the format BGRA (blue,green,red,alpha).
+     You can achieve this by implementing a for-loop that sets the values at each index.
+     I have not included a for-loop in this example because it depends on how the values are stored in your input 2D array.
+     You can set the alpha value to 255.
+     */
+    unsigned char pixelData[width * height];
+    for (int i=0; i<width*height; i++) {
+        pixelData[i]=i;
+    }
+    
+    // This is where the for-loop would be
+    
+    void *baseAddress = &pixelData;
+    
+    size_t bytesPerRow = width;
+    
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceGray();
+    CGContextRef context = CGBitmapContextCreate(baseAddress, width, height, 8, bytesPerRow, colorSpace, kCGBitmapByteOrderDefault);
+    CGImageRef cgImage = CGBitmapContextCreateImage(context);
+    CGContextRelease(context);
+    CGColorSpaceRelease(colorSpace);
+    return [UIImage imageWithCGImage:cgImage];
 }
 
 @end
